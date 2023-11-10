1f38abc548cc130add5089a9dd564b1debfacdda
Helmer <11061182+helmerdavila@users.noreply.github.com>
2023-11-10 00:17:42 -0500

    First preview of add request view

--- httpixel/Views/AddRequestView.swift
+++ httpixel/Views/AddRequestView.swift
@@ -1,10 +1,42 @@
 import SwiftUI
 
 struct AddRequestView: View {
+  @State private var url: String = ""
+  @State private var selectedHttpMethod: HTTPMethod = .get
+  
   var body: some View {
     VStack {
-      Text("This is the new request view")
+      List {
+        Section(header: Text("Request data"), content: {
+          TextField("URL", text: $url)
+          Picker("Method", selection: $selectedHttpMethod) {
+            ForEach(HTTPMethod.allCases, id: \.self) { method in
+              Text(method.rawValue).tag(method)
+            }
+          }
+        })
+        
+        
+        Button(action: {
+          print("Executing request")
+        }) {
+          Text("Execute Request")
+            .frame(maxWidth: .infinity)
+            .padding()
+        }
+        .buttonStyle(.bordered)
+        .controlSize(.regular)
+        .foregroundColor(Color.white)
+        .background(
+          RoundedRectangle(cornerRadius: 14.0, style: .continuous)
+            .fill(Color.primary)
+        )
+      }
+      .listStyle(.automatic)
+      
+      
     }
+    .navigationTitle(Text("New Request"))
   }
 }
 
